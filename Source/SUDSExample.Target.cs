2b716f75f8e0c0d5d09a271f2e9a454779d96a46
Steve Streeting <steve@stevestreeting.com>
2023-10-23 12:33:15 +0100

    Bring everything up to date with 5.3

--- Source/SUDSExample.Target.cs
+++ Source/SUDSExample.Target.cs
@@ -8,8 +8,8 @@ public class SUDSExampleTarget : TargetRules
 	public SUDSExampleTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
 		ExtraModuleNames.AddRange( new string[] { "SUDSExample" } );
 	}
--- Source/SUDSExampleEditor.Target.cs
+++ Source/SUDSExampleEditor.Target.cs
@@ -8,8 +8,8 @@ public class SUDSExampleEditorTarget : TargetRules
 	public SUDSExampleEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
 		ExtraModuleNames.AddRange( new string[] { "SUDSExample" } );
 	}
